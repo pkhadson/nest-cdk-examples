f9c23554e4aee7f2935e7bdf8c1e8349ba91b5c0
Patrick Hadson <pkhadson@gmail.com>
2023-12-11 12:05:20 -0300

    simple

---
+++ simple/lib/main.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@nest-cdk/core/register';
+import * as cdk from 'aws-cdk-lib';
+// import { RestApi } from 'aws-cdk-lib/aws-apigateway';
+// import { Construct } from 'constructs';
+
+const app = new cdk.App();
+
+// class AppStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);a
+
+//     // Define your API using AWS CDK constructs
+//     const api = new RestApi(this, 'MyApi');
+
+//     new NestStack(this, 'NestApplication', {
+//       project: 'nest-application',
+//       distPath: 'dist',
+//       api: api,
+//     });
+//   }
+// }
+
+// new AppStack(app, 'MyAppStack');
+
+defineConfig({
+  app,
+});
